968802bc5a6a0d429daac066d79ccae6ad7251cc
an.tran <an.tran@ANTRAN>
2024-04-10 08:07:39 +0700

    Add project files.

---
+++ src/Api/Controller/IdentityController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controller
+{
+    [Route("identity")]
+    [Authorize]
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var userClaims = User.Claims.Select(c => new { c.Type, c.Value }).ToList();
+            return new JsonResult(userClaims);
+        }
+    }
+}
---
+++ src/Api/Startup.cs
@@ -0,0 +1,42 @@
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+
+namespace Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "https://localhost:5001";
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = false
+                    };
+                });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
